abafc98f7e82adf91d224d44d392f255d83c06cc
mohamedhazem24 <mhazemcs@gmail.com>
2024-11-26 12:25:31 +0200

    delete_command

--- delete_table.sh
+++ delete_table.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+source './where.sh'
 
 function delete_from_table(){
-	
-	
-		
+	DBname=$1;
+    table_name=$2;
+    read -p "Enter Condition Column" colname;
+    read -p "Enter  the equality Column value" colval;
+    file_path="Databases/$1/$2";
+    coln=$(colmapping $DBname $table_name $colname)
+    echo "$coln"
+    if [[ $coln = -1 ]]; then
+    echo "Col not found";
+    exit;
+    fi;
+    
+    rown=$(rowmapping $DBname $table_name $coln $colval)
+    echo "$rown"
+    if [[ $rown = -1 ]]; then
+    echo "Row not found";
+    exit;
+    fi;
+
+    sed -i "${rown}d" "$file_path"
+	cat $file_path;
 }
+delete_from_table $1 $2
--- where.sh
+++ where.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
+
+function colmapping(){
+DBname=$1
+metafile=".meta$2"
+colname=$3
+file_path="Databases/$DBname/$metafile"
+# Use awk with -v to pass variables and capture the output
+result=$(awk -v val=$colname 'BEGIN{
+		FS=":"
+		row=-1
+	}{
+        if( $1 == val )
+        {
+            row = NR
+        }
+        
+
+	}END{
+        print(row)
+	}
+' $file_path)
+echo $result;
+
+}
 function rowmapping(){
-DBname=$$1
+DBname=$1
+table=$2
 col=$3
 val=$4
-
+file_path="Databases/$DBname/$table"
 # Use awk with -v to pass variables and capture the output
 result=$(awk -v colv=$col -v val=$val 'BEGIN{
 		FS=":"
@@ -18,8 +43,9 @@ result=$(awk -v colv=$col -v val=$val 'BEGIN{
 	}END{
         print(row)
 	}
-' $2)
+' $file_path)
 echo $result;
 
 }
-rowmapping 'hr' 'emmployees' 2 'osama'
+#coln=$(colmapping 'hr' 'emmployees' 'name')
+#rowmapping 'hr' 'emmployees' $coln 'hazem'
