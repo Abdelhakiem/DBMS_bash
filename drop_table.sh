f4682e732e59762a89ada066d3a2753470f38a81
Abdelhakiem <abdelhakim.osama150@gmail.com>
2024-11-26 20:15:48 +0200

    updated drop table, dropping metadata of the table

--- drop_table.sh
+++ drop_table.sh
@@ -6,9 +6,13 @@ function drop_table() {
 	
 	if [ -f "Databases/$DBName/$TableName" ]
 	then
+	# drop the table 
 		rm "Databases/$DBName/$TableName"
+	#drop metadata
+		rm "Databases/$DBName/.meta$TableName"
 		echo "Dropped Successfully"
 	else 
 		echo "Table does not exist"
 	fi
 }
+drop_table $1
